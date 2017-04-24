e39d33b5430e4d315f6e1a527ec308da87e229ae
marta <marta.voitanowych@gmail.com>
2017-04-24 17:30:35 +0300

    create thread

--- src/Counter.java
+++ src/Counter.java
@@ -1,9 +1,17 @@
+import java.io.File;
+import java.util.ArrayList;
+
 /**
  * Created by Марта on 24.04.2017.
  */
 public class Counter {
 
     public static void main(String[] args) {
-        
+        FileExcecutor fileExcecutor = new FileExcecutor();
+        String filename = "";
+        ArrayList<String> data =  fileExcecutor.convertFileDataToList(filename);
+        fileExcecutor.loadResultsForAllRespondentsInFile(data);
+
+
     }
 }
